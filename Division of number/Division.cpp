e6ab163a64c7aecee41b520d67446d4dd967661e
Rajatsaxena5487 <72753444+Rajatsaxena5487@users.noreply.github.com>
2020-10-27 12:32:06 +0530

    Add files via upload

---
+++ Division of number/Division.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+	
+  int a, b;
+  int c;
+  cout << "Enter the value of a: ";
+  cin >> a;
+  cout << "Enter the value of b: ";
+  cin >> b;
+  c= a / b;
+  cout << "Division: " <<  c;
+  return 0;
+}
+
