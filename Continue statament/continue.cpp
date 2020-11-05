5708c6470f29078e06a4f263793a12c690548407
RAJAT SAXENA <72753444+Rajatsaxena5487@users.noreply.github.com>
2020-11-05 23:09:01 +0530

    Add files via upload

---
+++ Continue statament/continue.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=0;
+	while(i<=10)
+	{
+         if(i==4)
+		 {
+		    i++;
+		 	continue;
+		
+				 }	
+				 else{
+				 	cout<<i<<"\n";
+				 	i++;
+				 }	
+		
+	
+	}
+	return 0;
+}
