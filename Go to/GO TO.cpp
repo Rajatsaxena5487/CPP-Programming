c4283f306ff06e8619cdc58eb3fde220adae0fc0
RAJAT SAXENA <72753444+Rajatsaxena5487@users.noreply.github.com>
2020-11-17 11:55:30 +0530

    Add files via upload

---
+++ Go to/GO TO.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+	 cout << "\nStatement 1.";
+	 cout << "\nStatement 2.";
+	 cout << "\nStatement 3.";
+     goto last;
+     cout << "\nStatement 4.";
+     cout << "\nStatement 5.";
+     last:
+	 cout << "\nEnd of Program.";
+  
+}
+
