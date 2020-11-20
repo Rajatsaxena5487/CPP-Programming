b5b1c9f7ccf49712d1c7bf094d1bac018b659aec
RAJAT SAXENA <72753444+Rajatsaxena5487@users.noreply.github.com>
2020-11-20 22:08:32 +0530

    Add files via upload

---
+++ Addition by class/class.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class A
+{
+	public :void add(int a,int b)
+	{
+		int c=a+b;
+		cout<<"Addition:="<<c;
+	}
+};
+
+int main()
+{
+	A ob;
+    int a,b;
+	cout<<"Enter the value of a&b";
+	cin>>a>>b;
+	ob.add(a,b);
+	return 0;
+}
